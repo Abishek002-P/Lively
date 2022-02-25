ef149233342b3bebf5dc3cf1a8c55b7fea75e00f
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-02-25 23:55:19 +0530

    Added livestreaming app

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected");
+  });
+
+  socket.on("call user", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("acceptcall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(5000, () => console.log("server is running on port 5000"));
